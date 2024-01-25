25c77d7a68521791c04c344b61e0acc4d1975d1b
mfaishal82 <mfaishal82@yahoo.co.id>
2024-01-25 11:45:48 +0700

    progress

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const session = require('express-session')
 
 // import controller
 const UserController = require('../controllers/UserController');
 
-router.get('/login', UserController.login)
-router.post('/login', UserController.verify)
-
 router.get('/register', UserController.register)
 router.post('/register', UserController.saveNewUser)
 
+router.get('/login', UserController.login)
+router.post('/login', UserController.verify)
+
+router.use(function (req, res, next) {
+    if(!req.session.UserId) {
+        const error = 'please login first'    
+        res.redirect(`/user/login?error=${error}`)
+    }
+    next()
+})
+
+
 router.get('/home', UserController.home)
 
+router.get('/logout', UserController.logout)
 
 
 module.exports = router;
